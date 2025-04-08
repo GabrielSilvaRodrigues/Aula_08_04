29ab4291ea14d5bbdf498a8d818224cac68a9147
Leonardo Quintino de Amorim <169057592+GabrielSilvaRodrigues@users.noreply.github.com>
2025-04-08 10:47:19 -0300

    Update

--- server_select.php
+++ server_select.php
@@ -7,9 +7,12 @@
     if($stmt){
         $stmt->execute([':email'=>$email]);
         $usuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if($usuario && password_verify($senha,$usuario(['senha']))){
-            $_SESSION['user_id'] = $usuario['id_usuario'];
-            $_SESSION['user_email'] = $usuario['email'];
+        if($usuario && password_verify($senha,$usuario[0]['senha'])){
+            $_SESSION['user_id'] = $usuario[0]['id_usuario'];
+            $_SESSION['user_email'] = $usuario[0]['email'];
+            echo'Login realizado com sucesso!';
+        } else{
+            echo 'Login ou senha incorretos!';
         }
     }
 ?>
