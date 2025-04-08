d15d1a057d62c8c3988cd1a7df23d76b7d946bc6
Leonardo Quintino de Amorim <169057592+GabrielSilvaRodrigues@users.noreply.github.com>
2025-04-08 10:14:46 -0300

    Update

---
+++ server_select.php
@@ -0,0 +1,15 @@
+<?php
+    $banco = new PDO('sqlite:banco_dados.db');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $sql = "SELECT * FROM usuario WHERE email=:email";
+    $stmt = $banco->prepare($sql);
+    if($stmt){
+        $stmt->execute([':email'=>$email]);
+        $usuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if($usuario && password_verify($senha,$usuario(['senha']))){
+            $_SESSION['user_id'] = $usuario['id_usuario'];
+            $_SESSION['user_email'] = $usuario['email'];
+        }
+    }
+?>
